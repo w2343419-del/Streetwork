3ba7d9ae99a30317c34b4f4765ebae11f6a57b92
w2343419@gmail.com <w2343419@gmail.com>
2025-10-31 12:38:38 +0800

    fix(10m29d/Code6.c): 添加计算数字各位数之和的功能
    新增了读取整数并计算其各位数字之和的逻辑，使用abs函数处理负数情况，
    通过while循环逐位提取数字并累加求和，最后输出结果。

--- 10m29d/Code6.c
+++ 10m29d/Code6.c
@@ -23,6 +23,18 @@
 3
 */ 
 #include <stdio.h>
+#include <stdlib.h>
 int main(){
-    
+	int n;
+	scanf("%d", &n);
+	n = abs(n);
+	int sum = 0;
+	
+	while (n != 0){
+		sum = sum + n % 10;
+		n = n / 10;
+	}
+	
+	printf ("%d", sum);
+	return 0;
 }
