c5d16ef05ada3f4e75148145e11f5613b0f004dc
w2343419@gmail.com <w2343419@gmail.com>
2025-12-27 13:13:46 +0800

    ``` feat(25y12m24d): 实现链表升序排序和位置查询功能
    重构了Code1.c文件，将原有的文件写入功能替换为链表数据结构实现。
    新增了链表节点定义和有序插入功能，实现输入n个数字构建升序链表，
    并能够查询指定位置m的节点值。同时添加了完整的内存释放机制，
    确保程序运行结束后正确释放所有分配的内存空间。
    ```

--- 25y12m24d/Code1.c
+++ 25y12m24d/Code1.c
@@ -1,22 +1,93 @@
-/*以键盘输入多行文字以EOF结束，将它写入到当前目录下面的“t1.txt”中保存。
-使用记事本打开该文件观察是否成功写入*/
+/*有如下的结构体：
+typedef struct Node  
+{  
+    int data;//存放数据
+    struct Node *pNext;//指向下一个结构体对象
+}NODE,*pNODE;
+现输入n（0<n<100）个数字，请用这n个整数构造出一个升序的链表，并打印出给定位置m（0<m<=n）的值；
+
+输入
+n
+N1 N2 N3 ... Nn
+m
+
+输出
+链表中第m个位置的data值
+
+样例
+输入
+2
+1 2
+1
+输出
+1
+
+输入
+4
+7 5 0 2
+3
+输出
+5*/
+
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <errno.h>
-extern int errno;
-int main() {
-    FILE *fp;
-    int ch;
-    fp = fopen("t1.txt", "w");
-    if (fp == NULL) {
-        printf ("Error: %s\n", strerror(errno));
-        return 1;
-    } else {
-        while ((ch = getchar()) != EOF) {
-            fputc(ch, fp);
-        }
+
+typedef struct Node {
+    int data;
+    struct Node *pNext;
+} NODE, *pNODE;
+
+pNODE insertSorted(pNODE pHead, int data) {
+    pNODE pNew = (pNODE)malloc(sizeof(NODE));
+    pNew->data = data;
+    pNew->pNext = NULL;
+    
+    if (pHead == NULL || pHead->data > data) {
+        pNew->pNext = pHead;
+        return pNew;
     }
-    fclose(fp);
+    
+    pNODE pCurrent = pHead;
+    while (pCurrent->pNext != NULL && pCurrent->pNext->data < data) {
+        pCurrent = pCurrent->pNext;
+    }
+    
+    pNew->pNext = pCurrent->pNext;
+    pCurrent->pNext = pNew;
+    
+    return pHead;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    pNODE pHead = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        scanf("%d", &num);
+        pHead = insertSorted(pHead, num);
+    }
+
+    int m;
+    scanf("%d", &m);
+
+    pNODE pCurrent = pHead;
+    for (int i = 1; i < m && pCurrent != NULL; i++) {
+        pCurrent = pCurrent->pNext;
+    }
+    
+    if (pCurrent != NULL) {
+        printf("%d\n", pCurrent->data);
+    }
+
+    pCurrent = pHead;
+    while (pCurrent != NULL) {
+        pNODE pTemp = pCurrent;
+        pCurrent = pCurrent->pNext;
+        free(pTemp);
+    }
+
     return 0;
 }
